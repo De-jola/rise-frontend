9167044394bb4fa246e651316a09e74ac782e6b8
Esther Adejola <adejolanu@gmail.com>
2026-01-24 01:05:42 +0100

    Updated example.txt and added screenshots

--- assignments/weather-dashboard/weather.sh
+++ assignments/weather-dashboard/weather.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 get_weatherData(){
+#get user input
 read -p "Enter a city e.g London, Italy etc: " city
 echo ""
 response=$(curl -s "https://wttr.in/${city}?format=j1")
 
-
+#handle errors
 if [ $? -ne 0 ]; then
 	echo "Please check your internet connection"
 	exit 1
@@ -21,6 +22,7 @@ if [ -z "$city" ]; then
 	exit 1
 fi
 
+#extract data points from API file
 temp=$(echo "$response" | grep -o '"temp_C": "[^"]*"' | head -1 | cut -d'"' -f4)
 cty_area=$(echo "$response" | grep -A 3 '"areaName"' | grep -o '"value": "[^"]*"' | head -1 | cut -d'"' -f4)
 condtn=$(echo "$response" | grep -o '"value": "[^"]*"' | head -1 | cut -d'"' -f4)
@@ -28,7 +30,7 @@ humidity=$(echo "$response" | grep -o '"humidity": "[^"]*"' | head -1 | cut -d'"
 wind_speed=$(echo "$response" | grep -o '"windspeedKmph": "[^"]*"' | head -1 | cut -d'"' -f4)
 
 
-
+#display extracted data to terminal
 echo "====Weather Information===="
 echo "City: $cty_area"
 echo "Temperature: ${temp}℃"
@@ -39,6 +41,7 @@ echo "============================"
 }
 get_weatherData
 
+#prompt user to look up another city
 while true; do
 	read -p "Check another city? (y/n): " c
 	case $c in
